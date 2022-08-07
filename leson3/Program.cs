8640c50ff60a3f043054cfa088109bba09825cb8
VladDul <dlekm1@mail.ru>
2022-08-08 01:16:01 +0300

    [tree1]exercise 1

--- leson3/Program.cs
+++ leson3/Program.cs
@@ -1,2 +1,9 @@
-﻿int a = 2;
-int b = 10;
+﻿Console.WriteLine("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+firstValue = firstValue + secondValue;
+secondValue = firstValue - secondValue;
+firstValue = firstValue - secondValue;
+Console.WriteLine($"Первое число равно:{firstValue},второе число равно:{secondValue}");
+
