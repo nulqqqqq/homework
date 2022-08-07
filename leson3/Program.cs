c759d90e5b84203702c1f183b83e0a2ecd6c2eab
VladDul <dlekm1@mail.ru>
2022-08-08 01:34:02 +0300

    [tree1]

--- leson3/Program.cs
+++ leson3/Program.cs
@@ -8,3 +8,29 @@ secondValue = firstValue - secondValue;
 firstValue = firstValue - secondValue;
 Console.WriteLine($"Первое число равно:{firstValue},второе число равно:{secondValue}");
 //задание 2
+void numberСomparison()
+{
+    Console.WriteLine("Введите первое число:");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine("Первое число больше второго");
+        numberСomparison();
+    }
+    if (firstNumber == secondNumber)
+    {
+        Console.WriteLine("Введенные числа равны");
+        numberСomparison();
+    }
+    if (firstNumber < secondNumber)
+    {
+        Console.WriteLine("Первое число меньше второго");
+        numberСomparison();
+    }
+}
+numberСomparison();
+//задание 3
+
+
