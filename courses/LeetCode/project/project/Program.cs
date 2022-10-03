5d860cd509dd1d34f6b8f0cc30dadba43e9a223e
VladDul <dlekm1@mail.ru>
2022-10-03 23:12:01 +0300

    exercise 3 compliete

--- courses/LeetCode/project/project/Program.cs
+++ courses/LeetCode/project/project/Program.cs
@@ -1,4 +1,5 @@
-﻿public static class Solution
+﻿Console.WriteLine(Solution.RomanToInt("MCMXCIV"));
+public static class Solution
 {
     //exercise 1:
     //Given a signed 32-bit integer x, return x with its digits reversed.
@@ -40,6 +41,55 @@
             }
         }
         return indexesOfValues;
+    }
+//exercise 3:
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//For example, 2 is written as II in Roman numeral, just two ones added together. 
+//12 is written as XII, which is simply X + II.The number 27 is written as XXVII, which is XX + V + II.
+//Roman numerals are usually written largest to smallest from left to right.
+//However, the numeral for four is not IIII. Instead, the number four is written as IV.
+//Because the one is before the five we subtract it making four.
+//same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+//I can be placed before V(5) and X(10) to make 4 and 9. 
+//X can be placed before L(50) and C(100) to make 40 and 90. 
+//C can be placed before D(500) and M(1000) to make 400 and 900.
+    public static int RomanToInt(string s)
+    {
+        int result = 0;
+        int y = 0;
+        Dictionary<string, int> a = new Dictionary<string, int>();
+        a.Add("I", 1);
+        a.Add("V", 5);
+        a.Add("X", 10);
+        a.Add("L", 50);
+        a.Add("C", 100);
+        a.Add("D", 500);
+        a.Add("M", 1000);
+
+        foreach(char i in s.ToCharArray())
+        {
+            int aa;
+            a.TryGetValue(i.ToString(), out aa);
+
+            if (y == 1 && (aa == 5||aa == 10))
+            {
+                result += aa-y*2; 
+            }
+            else if (y == 10 && (aa == 50 || aa == 100))
+            {
+                result += aa - y * 2;
+            }
+            else if (y == 100 && (aa == 500 || aa == 1000))
+            {
+                result += aa - y * 2;
+            }
+            else
+            {
+                result += aa;
+            }
+            y = aa;
+        }
+        return result;
     }
 
 }
