572fee4b65c711c7f2c8ce3c17d963677965037d
VladDul <dlekm1@mail.ru>
2022-10-07 22:25:05 +0300

    exercise completed!

---
+++ courses/lesson10/lesson10.5/lesson10.5/Director.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lesson10._5
+{
+    [AccessLevelAttribute(AttributeTargets.Enum, AccesLevelType.FullAccess)]
+    internal class Director:Employee
+    {
+
+    }
+}
---
+++ courses/lesson10/lesson10.5/lesson10.5/Program.cs
@@ -0,0 +1,58 @@
+﻿using lesson10._5;
+public enum AccesLevelType
+{
+    NonAccess,
+    PartAccess,
+    FullAccess
+}
+
+public class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Worker worker = new Worker();
+        Director director = new Director();
+        Manager manager = new Manager();
+        GetAccess(manager, AccesLevelType.FullAccess);
+        GetAccess(worker, AccesLevelType.NonAccess);
+        GetAccess(director, AccesLevelType.FullAccess);
+        
+        void GetAccess(Employee employee, AccesLevelType accesLevelType)
+        {
+            
+            Type type = employee.GetType();
+            object[] attributes = type.GetCustomAttributes(false);
+            foreach (Attribute attr in attributes)
+            {
+
+                if (attr is AccessLevelAttribute accessLevelAttribute)
+                {
+                    if (accessLevelAttribute.a >= accesLevelType)
+                    {
+                        Console.WriteLine("Доступ к данным разрешен");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Доступ к данным запрещен");
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+    public class Employee
+    {
+
+    }
+    public class AccessLevelAttribute : System.Attribute
+    {
+        public AccesLevelType a;
+        public AccessLevelAttribute(AttributeTargets attributeTargets, AccesLevelType accesLevelType)
+        {
+            a = accesLevelType;
+        }
+    }
+
