6ddc0894ac6998eda96fc6b38279e756d0231df1
VladDul <dlekm1@mail.ru>
2022-12-14 06:20:09 +0300

    add some fixes

--- courses/cursovaya/cursovaya/cursovaya/Data.cs
+++ courses/cursovaya/cursovaya/cursovaya/Data.cs
@@ -2,28 +2,22 @@
 {
     public class Data
     {
-        public List<Numbers<int>>    hotelNumberInt    = new List<Numbers<int>>();
-        public List<Numbers<double>> hotelNumberDouble = new List<Numbers<double>>();
+        public List<Numbers> hotelNumber = new List<Numbers>();
         public Data()
         {
-            hotelNumberInt.AddRange(new List<Numbers<int>>
+            hotelNumber.AddRange(new List<Numbers>
             {
-                new Numbers<int>(1, 101, 1, ServiceClass.FREE, 25),
-                new Numbers<int>(2, 102, 1, ServiceClass.FREE, 30),
-                new Numbers<int>(3, 103, 1, ServiceClass.FREE, 30),
-                new Numbers<int>(4, 104, 1, ServiceClass.FREE, 35),
-                new Numbers<int>(5, 105, 1, ServiceClass.FREE, 25),
-                new Numbers<int>(6, 201, 2, ServiceClass.FREE, 25),
-                new Numbers<int>(7, 202, 2, ServiceClass.FREE, 25)
+                new Numbers(1, 101, 1, MessageConstants.FREE, 25),
+                new Numbers(2, 102, 1, MessageConstants.FREE, 30),
+                new Numbers(3, 103, 1, MessageConstants.FREE, 30),
+                new Numbers(4, 104, 1, MessageConstants.FREE, 35),
+                new Numbers(5, 105, 1, MessageConstants.FREE, 25),
+                new Numbers(6, 201, 2, MessageConstants.FREE, 25),
+                new Numbers(7, 202, 2, MessageConstants.FREE, 35),
+                new Numbers(8, 203, 2, MessageConstants.FREE, 30),
+                new Numbers(9, 204, 2, MessageConstants.FREE, 25),
+                new Numbers(10, 205, 2, MessageConstants.FREE, 25)
             });
-
-            hotelNumberDouble.AddRange(new List<Numbers<double>>
-            {
-                new Numbers<double>(8, 203, 2, ServiceClass.FREE, 25.5),
-                new Numbers<double>(9, 204, 2, ServiceClass.FREE, 15.5),
-                new Numbers<double>(10, 205, 2, ServiceClass.FREE, 25.5) 
-            });
-            
         }
     }
 }
---
+++ courses/cursovaya/cursovaya/cursovaya/MessageConstants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursovaya
+{
+   public static class MessageConstants
+    {
+        public const string HELLO                     = "Здравствуйте ,как к вам обращаться?";
+        public const string WELCOME                   = "Добро пожаловать, {0} ";
+        public const string OPTIONS                   = "доступныые опции для вас:\n";
+        public const string FREE_NUMBERS              = "1.Cписок доступных номеров\n";
+        public const string ALL_NUMBERS               = "2.Cписок всех номеров\n";
+        public const string EXIT                      = "3.Выход";
+        public const string MISSING_OPTION            = "Такого варианта нет,пожалуйста выберете команду из доступных";
+        public const string SHOW_PRICE_OF_FREE_NUMBER = "номер {0} cвободен по цене: {1}$ за сутки";
+        public const string BACK_TO_OPTIONS           = "чтобы вернуться к опциям,нажмите 1";
+        public const string ORDER_NUMBER              = "чтобы забронировать номер,нажмите 2";
+        public const string SHOW_PRICE_AND_STATUS     = "цена на номер {0} = {1}$ в сутки. На данный момент:{2}";
+        public const string ENTER_DATE_OF_SETTLEMENT  = "Введите вашу дату заселения: ";
+        public const string ERROR_OF_DATE             = "неверно,введите дату в формате: 21.01.2022";
+        public const string PERSONAL_DATA             = "Чтобы заказать один из номеров ,потребуются ваши персональные данные";
+        public const string FNAME_AND_LNAME           = "Пожалуйста,напомните свое полное Имя и Фамилию:";
+        public const string ENTER_PHONE_NUMBER        = "введите свой номер телефона ";
+        public const string ENTER_CONT_OF_DAY         = "Введите количество дней проживания:";
+        public const string CONGRATULATIONS           = "Поздравляем, данный номер забронирован за вами.\n";
+        public const string GOOD_CHILL                = "Хорошего отдыха!";
+        public const string SELECT_HOTEL_NUMBER       = "Выберите номер комнаты для проживания из свободных на данный момент";
+        public const string TOTAL_PRICE               = "стоимсть этого номера будет {0}$";
+        public const string ENTER_LIKE_ADMIN          = "1.Войти как администратор\n";
+        public const string ENTER_LIKE_GUEST          = "2.Войти как гость";
+        public const string ERROR_OF_VARIANTS         = "Такого варианта нету ,выбирете из предоставленных:";
+        public const string GREETINGS_ADMIN           = "Вы вошли как администратор.Добро пожаловать!";
+        public const string INFORMATION_ABOUT_CLIENTS = "Информация о клиентах:";
+        public const string INFORMATION_ABOUT_ORDERS  = "{0} забронил комнату {1} на {2} дней";
+        public const string SAD_INFORMATION           = "На данный момент клиентов нет";
+        public const string FREE                      = "Свободен";
+        public const string BUSY                      = "Занят";
+    }
+}
